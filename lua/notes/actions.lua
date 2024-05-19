80457fd57ff912d7f22fb7aa2b357ca71cf454b8
nakatuba <nakatuba0626@gmail.com>
2024-05-19 17:20:45 +0900

    Keep normal mode after insert link

--- lua/notes/actions.lua
+++ lua/notes/actions.lua
@@ -5,10 +5,7 @@ function actions.insert_link(prompt_bufnr)
   actions.close(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   local note = selection.value
-  vim.schedule(function()
-    vim.cmd.startinsert()
-    vim.api.nvim_put({ '[' .. note.title .. '](' .. note.filename .. ')' }, '', true, true)
-  end)
+  vim.api.nvim_put({ '[' .. note.title .. '](' .. note.filename .. ')' }, '', true, true)
 end
 
 return actions
