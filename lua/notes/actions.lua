f817f8e2bf6dedf4a50201b1d389dc25a8265c6e
nakatuba <nakatuba0626@gmail.com>
2024-07-07 03:39:08 +0900

    Revert "Keep normal mode after insert link"
    This reverts commit 80457fd57ff912d7f22fb7aa2b357ca71cf454b8.

--- lua/notes/actions.lua
+++ lua/notes/actions.lua
@@ -5,7 +5,10 @@ function actions.insert_link(prompt_bufnr)
   actions.close(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   local note = selection.value
-  vim.api.nvim_put({ '[' .. note.title .. '](' .. note.filename .. ')' }, '', true, true)
+  vim.schedule(function()
+    vim.cmd.startinsert()
+    vim.api.nvim_put({ '[' .. note.title .. '](' .. note.filename .. ')' }, '', true, true)
+  end)
 end
 
 return actions
